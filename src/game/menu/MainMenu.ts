8587ec3f548d8e28c633d31408339df9d544130b
malibumango <166946644+malibumango@users.noreply.github.com>
2024-04-22 14:26:34 +0000

    fix: move menuPoints to constructor so that they do not duplicate
    Moved initialization of menuPoints to constructor because create()-method duplicates menuPoints

--- src/game/menu/MainMenu.ts
+++ src/game/menu/MainMenu.ts
@@ -1,69 +1,68 @@
-import { GameObjects } from "phaser";
-import Menu from "./Menu";
-import GameStateManager from "../GameStateManager";
+import { GameObjects } from 'phaser';
+import Menu from './Menu';
+import GameStateManager from '../GameStateManager';
 
 export class MainMenu extends Menu {
-  static SCENE_KEY = "MainMenu";
+  static SCENE_KEY = 'MainMenu';
   private logo: GameObjects.Image | undefined;
 
   constructor() {
-    super(MainMenu.SCENE_KEY, "background", "Main Menu");
+    super(MainMenu.SCENE_KEY, 'background', 'Main Menu');
+
+    this.menuPoints.push({
+      title: 'Start Game',
+      textStyle: {
+        fontFamily: 'Arial Black',
+        fontSize: 38,
+        color: '#ffffff',
+        stroke: '#000000',
+        strokeThickness: 4,
+        align: 'center',
+      },
+      origin: 0.5,
+      depth: 100,
+      onClick: () => {
+        this.changeSceneToGame();
+      },
+    });
+
+    this.menuPoints.push({
+      title: 'Credits',
+      textStyle: {
+        fontFamily: 'Arial Black',
+        fontSize: 38,
+        color: '#ffffff',
+        stroke: '#000000',
+        strokeThickness: 4,
+        align: 'center',
+      },
+      origin: 0.5,
+      depth: 100,
+      onClick: () => {
+        this.changeSceneToGameOverMenu();
+      },
+    });
+
+    this.menuPoints.push({
+      title: 'Options',
+      textStyle: {
+        fontFamily: 'Arial Black',
+        fontSize: 38,
+        color: '#ffffff',
+        stroke: '#000000',
+        strokeThickness: 4,
+        align: 'center',
+      },
+      origin: 0.5,
+      depth: 100,
+      onClick: () => {
+        this.changeSceneToGameOverMenu();
+      },
+    });
   }
 
   create() {
-    this.logo = this.add.image(512, 300, "logo").setDepth(100);
-
-    this.menuPoints.push({
-      title: "Start Game",
-      textStyle: {
-        fontFamily: "Arial Black",
-        fontSize: 38,
-        color: "#ffffff",
-        stroke: "#000000",
-        strokeThickness: 4,
-        align: "center",
-      },
-      origin: 0.5,
-      depth: 100,
-      onClick: () => {
-        this.changeSceneToGameOverMenu();
-      },
-    });
-
-    this.menuPoints.push({
-      title: "Credits",
-      textStyle: {
-        fontFamily: "Arial Black",
-        fontSize: 38,
-        color: "#ffffff",
-        stroke: "#000000",
-        strokeThickness: 4,
-        align: "center",
-      },
-      origin: 0.5,
-      depth: 100,
-      onClick: () => {
-        this.changeSceneToGameOverMenu();
-      },
-    });
-
-    this.menuPoints.push({
-      title: "Options",
-      textStyle: {
-        fontFamily: "Arial Black",
-        fontSize: 38,
-        color: "#ffffff",
-        stroke: "#000000",
-        strokeThickness: 4,
-        align: "center",
-      },
-      origin: 0.5,
-      depth: 100,
-      onClick: () => {
-        this.changeSceneToGameOverMenu();
-      },
-    });
-
+    this.logo = this.add.image(512, 300, 'logo').setDepth(100);
     this.addAllMenuPoints();
   }
 
